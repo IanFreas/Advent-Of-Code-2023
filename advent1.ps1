393aac77e3c102a075c7c2fa9030558241393725
Ian Freas <Ian.freas@gmail.com>
2023-12-17 18:26:47 -0800

    working on bugs still

--- advent1.ps1
+++ advent1.ps1
@@ -1,8 +1,8 @@
-[System.Collections.ArrayList]$output_array = @()
+[System.Collections.ArrayList]$outputArray = @()
 #$string = "two1nine"
-#$string_array = $string.ToCharArray()
+#$stringBrokenIntoArray = $string.ToCharArray()
 $strings = Get-Content -Path ".\advent1_input.txt"
-$number_mapping = @{
+$numberMatches = @{
 
     "one"   = 1
     "two"   = 2
@@ -14,48 +14,74 @@ $number_mapping = @{
     "eight" = 8
     "nine"  = 9
 }
-function Left_To_Right 
-{
-    #iterate for how long the given string is
-    #grow a new string by 1 char at at time
-    #perform 2 checks. Either if the growing string is an int or contains a word that spells a number
-    $test_array = $output_array
-
-    for ($i = 0; $i -lt $string_array.Count; $i++) 
-    {
-        #Take the index of the array and increment it on each pass. then grow the new string
-        $joined_string += $string_array[$i]
-    
-        #check 1: find if the joined string contains an integer
-        #Regex to find number in string as it grows
-        if ([regex]::Matches($joined_string, '\d+').Value) 
-        {
-            $output_array += [regex]::Matches($joined_string, '\d+').Value
-            $joined_string = '' #clear out the grown string
-            #break?
-        }
-        
-        #check 2: check if the growing string turns into a word that spells a number
-        elseif ($number_mapping.ContainsKey($joined_string)) {
-            $output_array.Add($number_mapping[$joined_string]) | Out-Null
-            $joined_string = '' # clear out the grown string
-            #exit
-        }
-
-        elseif ($test_array.Count -lt $output_array.Count) {
-            break
-        }
-    }
-}
+$expectedOutput = @(
+    2,
+    8,
+    1,
+    2,
+    4,
+    1,
+    7
+)
 
 #outer loop going through the whole file
 foreach ($string in $strings)
 {
     #Left to right check
-    Left_To_Right
+    #iterate for how long the given string is
+    #grow a new string by 1 char at at time
+    #perform 2 checks. Either if the growing string is an int or contains a word that spells a number
+    $originalArrayLength = $outputArray
+    $stringBrokenIntoArray = $string.ToCharArray()
+    
+    for ($i = 0; $i -lt $stringBrokenIntoArray.Count; $i++) 
+    {
+        #Take the index of the array and increment it on each pass. then grow the new string
+        $rebuiltString += $stringBrokenIntoArray[$i]
+    
+
+        
+        #check 1: find if the joined string contains an integer
+        #Regex to find number in string as it grows
+        if ([regex]::Matches($rebuiltString, '\d+').Value) 
+        {
+            $outputArray += [regex]::Matches($rebuiltString, '\d+').Value
+            $rebuiltString = '' #clear out the grown string
+            break
+        }
+        
+
+        #gotta check for wildcards here
+        #check 2: check if the growing string turns into a word that spells a number
+        elseif ($rebuiltString) {
+            foreach ($pattern in $numberMatches.Keys) {
+                if ($rebuiltString -like $pattern) {
+                    $outputArray.Add($numberMatches[$pattern]) | Out-Null
+                    $rebuiltString = '' # clear out the grown string
+                    break
+                }
+            }
+        }
+    
+        elseif ($originalArrayLength.Count -lt $outputArray.Count) 
+        {
+            break
+        }
+    }
 
     #Right to left check
 
 }
 
-$output_array
+$outputArray
+
+
+if ($testArray -eq $expectedOutput) 
+{
+    write-host "they match"
+}
+
+elseif ($testArray -ne $expectedOutput)
+{
+    write-host "dont match"
+}
