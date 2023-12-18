0f8d380a49a69d92b874ab4869e7f92218b6ae94
Ian Freas <Ian.freas@gmail.com>
2023-12-17 21:55:25 -0800

    brought back in the switch statement

--- advent1.ps1
+++ advent1.ps1
@@ -37,38 +37,70 @@ $script:expectedOutput =
     $script:originalArrayLength = $outputArray
     $script:stringBrokenIntoArray = $string.ToCharArray()
     
-    for ($i = 0; $i -lt $stringBrokenIntoArray.Count; $i++) 
+    :innerLoop for ($i = 0; $i -lt $stringBrokenIntoArray.Count; $i++) 
     {
         #Take the index of the array and increment it on each pass. then grow the new string
         $script:rebuiltString += $stringBrokenIntoArray[$i]
     
-
-        
         #check 1: find if the joined string contains an integer
         #Regex to find number in string as it grows
         if ([regex]::Matches($rebuiltString, '\d+').Value) 
         {
             $outputArray += [regex]::Matches($rebuiltString, '\d+').Value
             clear-variable -name rebuiltString #clear out the grown string
-            $i++
+            break innerLoop
         }
         
 
         #gotta check for wildcards here
         #check 2: check if the growing string turns into a word that spells a number
-        elseif ($rebuiltString) {
-            foreach ($pattern in $numberMatches.Keys) {
-                if ($rebuiltString -like $pattern) {
-                    $outputArray.Add($numberMatches[$pattern]) | Out-Null
-                    $rebuiltString = '' # clear out the grown string
-                    $i++
-                }
+        switch -Wildcard ($rebuiltString) 
+        {
+            "*one" 
+            {
+                $outputArray.Add(1) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+            }
+            "*two" 
+            {
+                $outputArray.Add(2) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+            }
+            "*three" 
+            {
+                $outputArray.Add(3) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+            }
+            "*four" 
+            {
+                $outputArray.Add(4) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+            }
+            "*five" 
+            {
+                $outputArray.Add(5) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+            }
+            "*six" 
+            {
+                $outputArray.Add(6) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+            }
+            "*seven" 
+            {
+                $outputArray.Add(7) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+            }
+            "*eight" 
+            {
+                $outputArray.Add(8) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
             }
         }
-    
-        elseif ($originalArrayLength.Count -lt $outputArray.Count) 
+        
+        if ($originalArrayLength.Count -lt $outputArray.Count) 
         {
-            $i++
+            break innerLoop
         }
     }
 
@@ -88,3 +120,5 @@ elseif ($testArray -ne $expectedOutput)
 {
     write-host "dont match"
 }
+
+gv rebuiltString, stringBrokenIntoArray, string | rv
