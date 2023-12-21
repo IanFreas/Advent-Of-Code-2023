d4696dda5d4b976faae42ce7479489eee3013c6e
Ian Freas <Ian.freas@gmail.com>
2023-12-20 20:02:50 -0800

    Challenge 2 completed!

--- advent1.ps1
+++ advent1.ps1
@@ -1,9 +1,10 @@
 [System.Collections.ArrayList]$outputArray = @()
-Set-Variable -Name outputArray -Scope script
 #$string = "two1nine"
 #$stringBrokenIntoArray = $string.ToCharArray()
-$script:strings = Get-Content -Path ".\advent1_input.txt"
-$script:numberMatches = 
+$strings = Get-Content -Path ".\advent1_input.txt"
+[System.Collections.ArrayList]$combinedParsedOutput = @()
+<#
+$numberMatches = 
 @{
 
     "one"   = 1
@@ -16,29 +17,29 @@ $script:numberMatches =
     "eight" = 8
     "nine"  = 9
 }
+#>
 
 #outer loop going through the whole file
-:outerLoop foreach ($script:string in $strings)
+:mainLoop foreach ($string in $strings)
 {
-    #Left to right check
     #iterate for how long the given string is
     #grow a new string by 1 char at at time
     #perform 2 checks. Either if the growing string is an int or contains a word that spells a number
-    $script:originalArrayLength = $outputArray
-    $script:stringBrokenIntoArray = $string.ToCharArray()
-    
-    :innerLoop for ($i = 0; $i -lt $stringBrokenIntoArray.Count; $i++) 
+    $stringBrokenIntoArray = $string.ToCharArray()
+    [System.Collections.ArrayList]$combinedParsedOutput = @()
+    #Left to right parse. Output to combinedParsedOutput
+    :parseLeftToRight for ($i = 0; $i -lt $stringBrokenIntoArray.Count; $i++) 
     {
         #Take the index of the array and increment it on each pass. then grow the new string
-        $script:rebuiltString += $stringBrokenIntoArray[$i]
+        $rebuiltString += $stringBrokenIntoArray[$i]
     
         #check 1: find if the joined string contains an integer
         #Regex to find number in string as it grows
         if ([regex]::Matches($rebuiltString, '\d+').Value) 
         {
-            $outputArray += [int]([regex]::Matches($rebuiltString, '\d+').Value)
+            $combinedParsedOutput += ([regex]::Matches($rebuiltString, '\d+').Value)
             clear-variable -name rebuiltString #clear out the grown string
-            break innerLoop
+            break parseLeftToRight
         }
         
 
@@ -48,64 +49,144 @@ $script:numberMatches =
         {
             "*one" 
             {
-                $outputArray.Add(1) | Out-Null
+                $combinedParsedOutput.Add(1) | Out-Null
                 clear-variable -name rebuiltString #clear out the grown string
-                break innerLoop
+                break parseLeftToRight
             }
             "*two" 
             {
-                $outputArray.Add(2) | Out-Null
+                $combinedParsedOutput.Add(2) | Out-Null
                 clear-variable -name rebuiltString #clear out the grown string
-                break innerLoop
+                break parseLeftToRight
             }
             "*three" 
             {
-                $outputArray.Add(3) | Out-Null
+                $combinedParsedOutput.Add(3) | Out-Null
                 clear-variable -name rebuiltString #clear out the grown string
-                break innerLoop
+                break parseLeftToRight
             }
             "*four" 
             {
-                $outputArray.Add(4) | Out-Null
+                $combinedParsedOutput.Add(4) | Out-Null
                 clear-variable -name rebuiltString #clear out the grown string
-                break innerLoop
+                break parseLeftToRight
             }
             "*five" 
             {
-                $outputArray.Add(5) | Out-Null
+                $combinedParsedOutput.Add(5) | Out-Null
                 clear-variable -name rebuiltString #clear out the grown string
-                break innerLoop
+                break parseLeftToRight
             }
             "*six" 
             {
-                $outputArray.Add(6) | Out-Null
+                $combinedParsedOutput.Add(6) | Out-Null
                 clear-variable -name rebuiltString #clear out the grown string
-                break innerLoop
+                break parseLeftToRight
             }
             "*seven" 
             {
-                $outputArray.Add(7) | Out-Null
+                $combinedParsedOutput.Add(7) | Out-Null
                 clear-variable -name rebuiltString #clear out the grown string
-                break innerLoop
+                break parseLeftToRight
             }
             "*eight" 
             {
-                $outputArray.Add(8) | Out-Null
+                $combinedParsedOutput.Add(8) | Out-Null
                 clear-variable -name rebuiltString #clear out the grown string
-                break innerLoop
+                break parseLeftToRight
+            }
+            "*nine" 
+            {
+                $combinedParsedOutput.Add(9) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseLeftToRight
             }
         }
-        
-        if ($originalArrayLength.Count -lt $outputArray.Count) 
+    }
+    
+    #Right to left parse. Output to combinedParsedOutput
+    :parseRightToLeft for ($i = 0; $i -lt $stringBrokenIntoArray.Count; $i++) 
+    {
+        #Take the index of the array and increment it on each pass. then grow the new string
+        #rebuilding in reverse
+        $rebuiltString += $stringBrokenIntoArray[-$i - 1]
+    
+        #check 1: find if the joined string contains an integer
+        #Regex to find number in string as it grows
+        if ([regex]::Matches($rebuiltString, '\d+').Value) 
         {
-            break innerLoop
+            $combinedParsedOutput += ([regex]::Matches($rebuiltString, '\d+').Value)
+            clear-variable -name rebuiltString #clear out the grown string
+            break parseRightToLeft
+        }
+        
+
+        #check 2: check if the growing string turns into a word that spells a number
+        #this time the word is reversed as it's built in reverse
+        switch -Wildcard ($rebuiltString) 
+        {
+            "*eno" 
+            {
+                $combinedParsedOutput.Add(1) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
+            "*owt" 
+            {
+                $combinedParsedOutput.Add(2) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
+            "*eerht" 
+            {
+                $combinedParsedOutput.Add(3) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
+            "*ruof" 
+            {
+                $combinedParsedOutput.Add(4) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
+            "*evif" 
+            {
+                $combinedParsedOutput.Add(5) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
+            "*xis" 
+            {
+                $combinedParsedOutput.Add(6) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
+            "*neves" 
+            {
+                $combinedParsedOutput.Add(7) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
+            "*thgie" 
+            {
+                $combinedParsedOutput.Add(8) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
+            "*enin" 
+            {
+                $combinedParsedOutput.Add(9) | Out-Null
+                clear-variable -name rebuiltString #clear out the grown string
+                break parseRightToLeft
+            }
         }
     }
-
-    #Right to left check
-
+    #take the combined values of each line and add it to the final array
+    $outputArray += [int]($combinedParsedOutput -join "")
 }
 
+#all values from each line and the total sum
 $outputArray
+write-host "total value:" ($outputArray | Measure-Object -sum).Sum
 
-gv rebuiltString, stringBrokenIntoArray, string | rv
+gv rebuiltString, stringBrokenIntoArray, string, combinedParsedOutput, outputArray | rv
