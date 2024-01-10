a649ffa96d6631dbbaddc12c70de3055b35ac58d
Ian Freas <Ian.freas@gmail.com>
2024-01-09 21:08:57 -0800

    idk man

--- day2.ps1
+++ day2.ps1
@@ -5,8 +5,6 @@
 # once done check if the game is possible then add that game number to a new array
 # get the sum of that array for the answer
 
-
-
 $intputFile = (get-location).path + "\day2_input.txt"
 $fileContents = Get-Content $intputFile
 $myHash = @{}
@@ -19,7 +17,6 @@ $limits = 12,13,14
 
 # Main for loop
 for ($i = 0; $i -lt $fileContents.Count; $i++){
- 
     # split the game with number string out first     
     $gameInputSplit = $fileContents[$i] -split ':'
     $gameNumber = $gameInputSplit[0]
@@ -29,14 +26,13 @@ for ($i = 0; $i -lt $fileContents.Count; $i++){
     
     # Some regex to grab the number of the cube rolls
     $cubeRolls = [regex]::Matches($gameOutputs, '\b\d+\b') | ForEach-Object {$_.Value}
-   
-    if ($cubeRolls -le $limits){
 
-	#fix by checking one limit at at time	
+    foreach ($limit in $limits){
+	if ($cubeRolls -ge $limit){
+    	    Write-Host "$cubeRolls is greater than $limit"
+        }
     }
- 
     #$myhash.add($gameNumber,$cubeRolls)
-
 }
 
 #make a red limit loop - break if hit
