aaf1d810758f83d8e11a0aaaaf34a90528c12181
Ian Freas <Ian.freas@gmail.com>
2024-01-27 22:27:40 -0800

    almost fixed now
    need to get the switch statement default to work

--- day2.ps1
+++ day2.ps1
@@ -28,14 +28,75 @@ for ($i = 0; $i -lt $fileContents.Count; $i++)
     $sortedArray = $array | ForEach-Object {[int]$_} | Sort-Object -Descending
      
 
-    if ($sortedArray[0] -le 12) {
+    if ($sortedArray[0] -le 12)
+    {
         $total += $number
-        write-host "$gameNumber is possible"
     }
+
+    $gameContentsAgain = (($gameContents -split ',').Trim() -split ';').trim()
+    
+    switch -Wildcard ($gameContentsAgain) {
+        "*red" {
+            $red = [int]([regex]::matches($_, '\d+').value)
+            if ($red -ge $redLimit)
+            {
+                break
+            }
+          }
+        "*green" {
+            $green = [int]([regex]::matches($_, '\d+').value)
+            if ($green -ge $greenLimit)
+            {
+                break
+            } 
+         }
+        "*blue" { 
+            $blue = [int]([regex]::matches($_, '\d+').value)
+            if ($blue -ge $blueLimit)
+            {
+                break
+            }
+         }
+        Default {$total += $number}
+    }
+
+switch ($x) {
+    condition {  }
+    Default {}
+}
+
+<#
+    $gameContentsAgain | ForEach-Object {
+        if ($_ -match 'red')
+        {
+            $red = [int]([regex]::matches($_, '\d+').value)
+            if ($red -ge $redLimit)
+            {
+                break
+            }
+        }
+        elseif ($_ -match 'green')
+        {
+            $green = [int]([regex]::matches($_, '\d+').value)
+            if ($green -ge $greenLimit)
+            {
+                break
+            }            
+        }
+        elseif ($_ -match 'blue')
+        {
+            $blue = [int]([regex]::matches($_, '\d+').value)
+            if ($blue -ge $blueLimit)
+            {
+                break
+            }
+        }
+        
+    }
+
     else {
         :innerLoop for ($j = 0; $j -lt $sortedArray.Count; $j++)
         {
-            <# Action that will repeat until the condition is met #>
             if (($sortedArray[$j]) -le $redLimit) 
             {
                 $total += $number
@@ -59,5 +120,6 @@ for ($i = 0; $i -lt $fileContents.Count; $i++)
             }  
         }
     }
+    #>
     $total
 }
